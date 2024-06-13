4792e76302863934a4dee13a1266681f4651d22f
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-13 12:37:52 +0330

    accounts views - Created RegisterView
    Created RegisterView as a From CBV for registration of new users

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from django.contrib.auth import login
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class RegisterView(FormView):
+    """
+    Managing registration of new users
+
+    Attributes:
+     - email: User's valid email address
+     - password1: User's password
+     - password2: User's password confirmation
+    """
+
+    template_name = "registration/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("blog:index")
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            # login user after successful registration
+            login(self.request, user)
+        return super().form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        return super().get(request, *args, **kwargs)
