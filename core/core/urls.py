a525dea39474988c31b16b4a06d9ed1d66250271
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-13 09:20:21 +0330

    core URL - blog app URL
    Included blog app URLs in the project's core URLs

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -37,6 +38,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path("", include("blog.urls")),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path(
