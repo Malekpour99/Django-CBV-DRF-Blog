da4b689e3338794b2faddec1dff014615d9a5120
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-18 10:54:38 +0330

    blog admin - Registering post & category models
    Registered Post and Category models in the admin panel for viewing and editing them

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from blog.models import Post, Category
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    empty_value_display = "-None-"
+    list_display = (
+        "title",
+        "published_at",
+        "author",
+        "category",
+        "counted_views",
+        "published_status",
+        "created_at",
+    )
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("published_status", "author", "category")
+    search_fields = ["title", "content"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
