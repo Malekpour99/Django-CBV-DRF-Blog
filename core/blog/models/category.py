5a5df3b266a07b975cd7173cc0d61e05b35d237e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-18 10:53:11 +0330

    blog models - Created Category model
    Created Category model for managing blog post categories

---
+++ core/blog/models/category.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.db import models
+from django.utils.text import slugify
+
+from .base import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # ensuring names uniqueness for any database
+        self.name = self.name.lower()
+        if not self.slug:
+            self.slug = slugify(self.name)
+        if Category.objects.filter(slug=self.slug).exists():
+            self.slug = f"{self.slug}-{uuid.uuid4().hex[:8]}"
+        super().save(*args, **kwargs)
