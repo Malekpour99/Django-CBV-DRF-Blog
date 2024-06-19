4c992e2aaff94185fe0a407038f6f82e72207e67
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-19 17:31:26 +0330

    accounts models - Profile model username field
    Added a username field for the user's profile to show this name for our user as the author of the blog post

--- core/accounts/models/profiles.py
+++ core/accounts/models/profiles.py
@@ -1,11 +1,15 @@
+import uuid
+
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+
 from .users import User
 
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=250, unique=True)
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     image = models.ImageField(
@@ -15,6 +19,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.email
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.user.email.split("@")[0]
+        if Profile.objects.filter(username=self.username).exists():
+            self.username = f"{self.username}-{uuid.uuid4().hex[:8]}"
+        super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=User)
