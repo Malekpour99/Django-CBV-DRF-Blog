77d60d01343d45ca4d1910e2eee7dc8a68ae57b2
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-06-26 06:28:28 +0330

    feat: blog single post view, URL, template & CSS styles
    Moved all the logic related to blog single post to a separate branch for further development and bug fix

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 
@@ -11,3 +11,12 @@ class PostListView(ListView):
     template_name = "blog/index.html"
     queryset = Post.objects.select_related("author").filter(published_status=True)
     context_object_name = "posts"
+
+
+class PostDetailView(DetailView):
+    """
+    Showing details of a published post
+    """
+
+    model = Post
+    template_name = "blog/post.html"
