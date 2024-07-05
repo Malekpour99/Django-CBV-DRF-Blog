d8be15c3e6ad169df8e109442975bd4bae6c43a2
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-07-05 11:48:03 +0330

    feat: blog - created blog post author URL path
    This URL path will be used to filter blog posts based on their author if the author's username is provided

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -5,6 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="index"),
-    path("category/<slug:cate_slug>", views.PostListView.as_view(), name="category"),
+    path("author/<str:author_username>", views.PostListView.as_view(), name="author"),
+    path("category/<slug:cat_slug>", views.PostListView.as_view(), name="category"),
     path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
 ]
