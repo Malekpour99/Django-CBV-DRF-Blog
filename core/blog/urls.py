25c06bdd964aa7435651fd6ee2e6d56be9a9233e
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-07-05 11:26:31 +0330

    feat: blog - added category URL path
    Created a new URL path for filtering posts based on their category

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -5,5 +5,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.PostListView.as_view(), name="index"),
+    path("category/<slug:cate_slug>", views.PostListView.as_view(), name="category"),
     path("post/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
 ]
