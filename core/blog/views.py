7b0ff8db31d8f34b7c67a5aa6253d5a63c67a6c1
Amirhosein Malekpour <malekpour1999@gmail.com>
2024-07-06 23:22:40 +0330

    feat: blog - created SearchView for blog posts
    By using search view, now users can search through blog post titles and contents to find what they are looking for and also a page-title context will be shown to them in order to provide better UX

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+from django.db.models import Q
 from django.utils import timezone
+from django.shortcuts import render
 from django.db.models.query import QuerySet
-from django.views.generic import ListView, DetailView
+from django.views.generic import View, ListView, DetailView
 
 from blog.models import Post
 
@@ -26,6 +28,22 @@ class PostListView(ListView):
         if self.kwargs.get("author_username"):
             posts = posts.filter(author__username=self.kwargs["author_username"])
         return posts
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.kwargs.get("cat_slug"):
+            category = self.kwargs["cat_slug"]
+            page_title = f"Posts in '{category.capitalize()}' category"
+        elif self.kwargs.get("author_username"):
+            author = self.kwargs["author_username"]
+            page_title = f"Posts by '{author.capitalize()}'"
+        else:
+            page_title = "Recent Posts"
+
+        context["page_title"] = page_title
+
+        return context
 
 
 class PostDetailView(DetailView):
@@ -35,3 +53,21 @@ class PostDetailView(DetailView):
 
     model = Post
     template_name = "blog/post.html"
+
+
+class SearchView(View):
+    """
+    Searching blog posts
+    """
+
+    def get(self, request, *args, **kwargs):
+        search_query = self.request.GET.get("search", "")
+        posts = Post.objects.filter(
+            Q(content__icontains=search_query) | Q(title__icontains=search_query)
+        ).order_by("-published_at")
+        page_title = "Search results for: " + '"' + search_query + '"'
+        context = {
+            "page_title": page_title,
+            "posts": posts,
+        }
+        return render(request, "blog/index.html", context)
