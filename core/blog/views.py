dcc48163f48db1cd3ad0465edb4c3f12dc494222
Amirhosein Malekpour <work.malekpour99@gmail.com>
2024-07-26 00:21:33 +0330

    feat: created PostPublishView for publishing new posts
    PostPublishView is a based on CreateView class from Django generic views which enables users to publish their own blog posts

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,11 +2,14 @@ from typing import Any
 
 from django.db.models import Q
 from django.utils import timezone
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.db.models.query import QuerySet
-from django.views.generic import View, ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View, ListView, DetailView, CreateView
 
-from blog.models import Post
+
+from blog.models import Post, Category
 
 
 class PostListView(ListView):
@@ -29,11 +32,11 @@ class PostListView(ListView):
         if self.kwargs.get("author_username"):
             posts = posts.filter(author__username=self.kwargs["author_username"])
         return posts
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         is_home = True
-        
+
         if self.kwargs.get("cat_slug"):
             category = self.kwargs["cat_slug"]
             page_title = f"Posts in '{category.capitalize()}' category"
@@ -77,3 +80,35 @@ class SearchView(View):
             "is_home": False,
         }
         return render(request, "blog/index.html", context)
+
+
+class PostPublishView(LoginRequiredMixin, CreateView):
+    """
+    Provides a form for users to publish a new post
+    """
+
+    template_name = "blog/post_publish.html"
+    model = Post
+    fields = [
+        "image",
+        "title",
+        "content",
+        "category",
+        "published_at"
+    ]
+    success_url = reverse_lazy("blog:index")
+
+    def form_valid(self, form):
+        form.instance.author_id = self.request.user.id
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page_title = "Publish a new Post"
+        categories = Category.objects.filter(is_deleted=False).order_by("name")
+
+        context["page_title"] = page_title
+        context["categories"] = categories
+
+        return context
+
