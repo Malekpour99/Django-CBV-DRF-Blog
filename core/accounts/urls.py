38dbc8c83064a5ef6f3d1d7673d4902c6727a1d6
Amirhosein Malekpour <work.malekpour99@gmail.com>
2024-08-16 21:25:20 +0330

    feat: accounts URLs - added profile URL path
    added a URL path for the profile view to let users view their profiles

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from .views import RegisterView, ProfileView
 
 app_name = "accounts"
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(redirect_authenticated_user=True), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
+    path("profile/<str:username>/", ProfileView.as_view(), name="profile"),
     path("api/v1/", include("accounts.api.v1.urls")),
 ]
