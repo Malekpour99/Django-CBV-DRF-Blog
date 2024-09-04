58a6dce96eb17a3c39b6c4b06bc2d9a50767ba04
Amirhosein Malekpour <work.malekpour99@gmail.com>
2024-09-04 19:47:23 +0330

    feat: blog utils - created fetch_user_posts staticmethod
    added fetch_user_posts to the BlogPostHandler utility class for fetching all the user's created posts excluding deleted ones

--- core/blog/utils.py
+++ core/blog/utils.py
@@ -19,3 +19,17 @@ class BlogPostHandler:
             is_deleted=False,
             published_at__lte=timezone.now(),
         ).order_by("-published_at")
+
+    @staticmethod
+    def fetch_user_posts(user_id):
+        """
+        Fetches and returns user's all blog posts, which are
+        not deleted
+
+        Returns:
+            QuerySet[Post]: A queryset of user's blog posts.
+        """
+        return Post.objects.filter(
+            author__id=user_id,
+            is_deleted=False,
+        ).order_by("-published_at")
