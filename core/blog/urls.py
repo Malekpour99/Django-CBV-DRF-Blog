0a05d1041072bd5107ba41092638995dee786042
Amirhosein Malekpour <work.malekpour99@gmail.com>
2024-09-04 19:58:54 +0330

    refactor: blog URLs - added blog/ to all URL paths
    in order to provide more informative URL paths and prevent URLs from clashing

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,11 +6,11 @@ app_name = "blog"
 urlpatterns = [
     path("", views.PostListView.as_view(), name="index"),
     path("blog/your-posts/", views.UserPostsListView.as_view(), name="user-posts"),
-    path("author/<str:author_username>", views.PostListView.as_view(), name="author"),
-    path("category/<slug:cat_slug>", views.PostListView.as_view(), name="category"),
-    path("posts/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("search/", views.SearchView.as_view(), name="search"),
-    path("post-publish/", views.PostPublishView.as_view(), name="post-publish"),
-    path("category-publish/", views.CategoryPublishView.as_view(), name="category-publish"),
+    path("blog/author/<str:author_username>/", views.PostListView.as_view(), name="author"),
+    path("blog/category/<slug:cat_slug>/", views.PostListView.as_view(), name="category"),
+    path("blog/posts/<slug:slug>/", views.PostDetailView.as_view(), name="post-detail"),
     path("blog/your-posts/<slug:slug>/", views.UserPostDetailView.as_view(), name="user-post-detail"),
+    path("blog/search/", views.SearchView.as_view(), name="search"),
+    path("blog/post-publish/", views.PostPublishView.as_view(), name="post-publish"),
+    path("blog/category-publish/", views.CategoryPublishView.as_view(), name="category-publish"),
 ]
