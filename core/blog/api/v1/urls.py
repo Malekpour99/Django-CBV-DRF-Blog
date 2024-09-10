f26a67c59ca7316bda426d9f0be73e692d62d7dd
Malekpour99 <work.malekpour99@gmail.com>
2024-09-10 15:49:36 +0330

    feat: blog API URLs - registered categories in the default router
    registered CategoryModelViewSet as categories in the default router

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -6,4 +6,5 @@ app_name = "api-v1"
 
 router = DefaultRouter()
 router.register("posts", views.PostModelViewSet, basename="posts")
+router.register("categories", views.CategoryModelViewSet, basename="categories")
 urlpatterns = router.urls
