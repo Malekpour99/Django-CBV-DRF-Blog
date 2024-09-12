6ac1f880b5a58ad8ec6103f54a6401e481bbf280
Amirhosein Malekpour <work.malekpour99@gmail.com>
2024-09-12 12:19:05 +0330

    feat: comments models - created Comment model
    created Comment model which is used for managing blog post comments created by users

--- core/comment/models.py
+++ core/comment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from blog.models import Post
+from accounts.models import Profile
+from blog.models.base import BaseModel
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    commenter = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name="user_comments"
+    )
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.commenter.username} - {self.subject}"
